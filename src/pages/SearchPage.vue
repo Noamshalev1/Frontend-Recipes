c8d2a255aef18b3312c75a447dcbcfdef58adb0a
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2024-06-09 15:12:03 +0300

    Fix API calls

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -6,26 +6,26 @@
         <b-input-group-prepend is-text>
           <b-icon icon="search"></b-icon>
         </b-input-group-prepend>
-        <b-form-input v-model="searchQuery" placeholder="type to search for recipie" @keyup.enter="performSearch"></b-form-input>
+        <b-form-input v-model="searchQuery" placeholder="Type to search for recipe" @keyup.enter="performSearch"></b-form-input>
         <b-input-group-append>
           <b-button variant="info" @click="performSearch">Search</b-button>
         </b-input-group-append>
         <template #append>
           <b-dropdown :text="`Number of Recipes: ${resultsCount}`" variant="secondary">
             <b-dropdown-group header="Choose option" class="small">
-            <b-dropdown-item-button @click="updateResultsCount(5)">
-              <b-icon :icon="resultsCount === 5 ? 'check' : 'blank'" aria-hidden="true"></b-icon>
-              5 <span class="sr-only"></span>
-            </b-dropdown-item-button>
-            <b-dropdown-item-button @click="updateResultsCount(10)">
-              <b-icon :icon="resultsCount === 10 ? 'check' : 'blank'" aria-hidden="true" ></b-icon>
-              10 <span class="sr-only"></span>
-            </b-dropdown-item-button>
-            <b-dropdown-item-button @click="updateResultsCount(15)">
-              <b-icon :icon="resultsCount === 15 ? 'check' : 'blank'" if  aria-hidden="true"></b-icon>
-              15 <span class="sr-only"></span>
-            </b-dropdown-item-button>
-          </b-dropdown-group>
+              <b-dropdown-item-button @click="updateResultsCount(5)">
+                <b-icon :icon="resultsCount === 5 ? 'check' : 'blank'" aria-hidden="true"></b-icon>
+                5 <span class="sr-only"></span>
+              </b-dropdown-item-button>
+              <b-dropdown-item-button @click="updateResultsCount(10)">
+                <b-icon :icon="resultsCount === 10 ? 'check' : 'blank'" aria-hidden="true"></b-icon>
+                10 <span class="sr-only"></span>
+              </b-dropdown-item-button>
+              <b-dropdown-item-button @click="updateResultsCount(15)">
+                <b-icon :icon="resultsCount === 15 ? 'check' : 'blank'" aria-hidden="true"></b-icon>
+                15 <span class="sr-only"></span>
+              </b-dropdown-item-button>
+            </b-dropdown-group>
           </b-dropdown>
         </template>
       </b-input-group>
@@ -56,17 +56,17 @@
       </b-input-group>
     </div>
     <div class="recipes">
-    <div v-if="searchQuery.length === 0">
-          <!-- Show nothing if there's no query -->
+      <div v-if="searchQuery.length === 0">
+        <!-- Show nothing if there's no query -->
+      </div>
+      <div class="row" v-else-if="filteredRecipes.length">
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4" v-for="recipe in limitedFilteredRecipes" :key="recipe.id">
+          <RecipePreview :recipe="recipe" />
         </div>
-        <div class="row" v-else-if="filteredRecipes.length">
-          <div class="col-lg-4 col-md-6 col-sm-12 mb-4" v-for="recipe in limitedFilteredRecipes" :key="recipe.id">
-            <RecipePreview :recipe="recipe" />
-          </div>
-        </div>
-        <div v-else>
-          <p>No recipes found. Please enter a search query.</p>
-        </div>
+      </div>
+      <div v-else>
+        <p>No recipes found. Please enter a search query.</p>
+      </div>
     </div>
   </div>
 </template>
@@ -85,11 +85,21 @@ export default {
       recipes: [],
       filteredRecipes: [],
       resultsCount: 5,
-      cuisines: [],
+      cuisines: [
+        'African', 'American', 'British', 'Japanese', 'Jewish',
+        'Mexican', 'Spanish', 'Thai', 'Vietnamese', 'Nordic',
+        'European', 'French'
+      ],
       selectedCuisine: null,
-      diets: [],
+      diets: [
+        'Gluten Free', 'Ketogenic', 'Vegetarian', 'Lacto-Vegetarian',
+        'Ovo-Vegetarian', 'Vegan', 'Pescetarian', 'Paleo', 'Whole30'
+      ],
       selectedDiet: null,
-      intolerances: [],
+      intolerances: [
+        'Dairy', 'Egg', 'Gluten', 'Peanut', 'Seafood',
+        'Sesame', 'Shellfish', 'Soy', 'Sulfite', 'Tree Nut', 'Wheat'
+      ],
       selectedIntolerances: [],
       sortOption: 'popularity', // Default sort option
       sortDirection: 'desc' // Default sort direction
@@ -103,56 +113,15 @@ export default {
   created() {
     this.recipes = recipes;
     this.loadLastSearch();
-    this.fetchCuisines();
-    this.fetchDiets();
-    this.fetchIntolerances();
-    },
+  },
   methods: {
     changeSort(option) {
-    if (this.sortOption === option) {
-    } else {
+      if (this.sortOption === option) {
+        // If already selected, do nothing
+        return;
+      }
       this.sortOption = option;
-    }
-    this.performSearch(); // Re-run the search with the new sort settings
-    },
-    fetchCuisines() {
-      fetch('https://api.spoonacular.com/recipes/cuisine', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Api-Key': 'b1a72f1616ff413e984ea8dc1377d964'
-        }
-      })
-        .then(response => response.json())
-        .then(data => {
-          this.cuisines = data.cuisines;
-        });
-    },
-    fetchDiets() { //NEED TO GO OVER THIS !!! <------------------------------------------------------------------------------
-      fetch('https://api.spoonacular.com/recipes/diet', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Api-Key': 'b1a72f1616ff413e984ea8dc1377d964'
-        }
-      })
-        .then(response => response.json())
-        .then(data => {
-          this.diets = data.diets;
-        });
-    },
-    fetchIntolerances() { //NEED TO GO OVER THIS !!! <------------------------------------------------------------------------------
-      fetch('https://api.spoonacular.com/recipes/intolerances', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Api-Key': 'b1a72f1616ff413e984ea8dc1377d964'
-        }
-      })
-        .then(response => response.json())
-        .then(data => {
-          this.intolerances = data.intolerances;
-        });
+      this.performSearch(); // Re-run the search with the new sort settings
     },
     performSearch() {
       this.searchQuery = this.searchQuery.toLowerCase();
@@ -167,38 +136,37 @@ export default {
           } else {
             return recipe.title.toLowerCase().includes(this.searchQuery) ||
                    recipe.summary.toLowerCase().includes(this.searchQuery);
-            
           }
         });
         if (this.selectedCuisine) {
-          filtered = filtered.filter(recipe => recipe.cuisine === this.selectedCuisine);
+          this.filteredRecipes = this.filteredRecipes.filter(recipe => recipe.cuisine === this.selectedCuisine);
         }
         if (this.selectedDiet) {
-          filtered = filtered.filter(recipe => recipe.diet === this.selectedDiet);
+          this.filteredRecipes = this.filteredRecipes.filter(recipe => recipe.diet === this.selectedDiet);
         }
         if (this.selectedIntolerances.length) {
-          filtered = filtered.filter(recipe =>
+          this.filteredRecipes = this.filteredRecipes.filter(recipe =>
             this.selectedIntolerances.every(intolerance => recipe.intolerances.includes(intolerance))
           );
         }
         this.filteredRecipes.sort((a, b) => {
-        if (this.sortOption === 'popularity') {
-          return this.sortDirection === 'asc' ? a.aggregateLikes - b.aggregateLikes : b.aggregateLikes - a.aggregateLikes;
-        } else if (this.sortOption === 'prepTime') {
-          return this.sortDirection === 'asc' ? a.readyInMinutes - b.readyInMinutes : b.readyInMinutes - a.readyInMinutes;
-        }
-        return 0;
-  });
+          if (this.sortOption === 'popularity') {
+            return this.sortDirection === 'asc' ? a.aggregateLikes - b.aggregateLikes : b.aggregateLikes - a.aggregateLikes;
+          } else if (this.sortOption === 'prepTime') {
+            return this.sortDirection === 'asc' ? a.readyInMinutes - b.readyInMinutes : b.readyInMinutes - a.readyInMinutes;
+          }
+          return 0;
+        });
         this.saveLastSearch(this.searchQuery);
       } else {
         this.filteredRecipes = [];
       }
     },
-    saveLastSearch() {
-      localStorage.setItem('lastSearch', this.searchQuery);
+    saveLastSearch(query) {
+      localStorage.setItem("lastSearch", query);
     },
     loadLastSearch() {
-      const lastSearch = localStorage.getItem('lastSearch');
+      const lastSearch = localStorage.getItem("lastSearch");
       if (lastSearch) {
         this.searchQuery = lastSearch;
         this.performSearch();
@@ -217,14 +185,12 @@ export default {
       }
       this.performSearch();
     },
-  }
+  },
 };
 </script>
 
 <style scoped>
-
 .mt-3 {
   margin-bottom: 20px;
 }
-
 </style>
